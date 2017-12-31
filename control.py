b966e4a1768713944d0f3d4bfb5f3a8fa2986dd1
Hector Torres <blackarrow06@users.noreply.github.com>
2017-12-31 17:41:06 -0600

    Added Twitter account information to add detail to ID strings used as inputs to the twitter_follow_collector

--- control.py
+++ control.py
@@ -55,6 +55,35 @@ keywords = ['international relations', 'political science', 'international affai
 """
 sets up the variables for Twitter follow collection: this is a list of Twitter user IDs. Per the Twitter docs: A comma-
 separated list of user IDs, indicating the users whose Tweets should be delivered on the stream.
+
+        Twitter IDs can be found at sites such as http://mytwitterid.com
+        1.  @business           '34713362'          Bloomberg
+        2.  @cnnbrk             '428333'            CNN Breaking News
+        3.  @cbstopnews         '18767649'          CBS Top News
+        4.  @wsjbreakingnews    '23484039'          WSJ Breaking News
+        5.  @ftcommodities      '346569891'         FT Commodities
+        6.  @bncommodities      '804556370'         BN Commodities
+        7.  @reuterescommods    '81075524'          Reuters Commodities
+        8.  @AP                 '51241574'          AP
+        9.  @conflicts          '2566535282'        Conflict News
+        10. @afp                '380648579'         AFP News Agency
+        11. @bnonews            '189305014'         BNO News
+        12. @SkyNewsBreak       '87416722'          Sky News Newsdesk
+        13. @USGSBigQuakes      '94119095'          USGS Big Quakes         USGS earthquake alerts
+        14. @CNBCnow            '26574283'          CNBC Now
+        15. @AJENews            '18424289'          Al Jazeera News
+        16. @UPI                '16666806'          UPI.com
+        17. @BBCBreaking        '5402612'           BBC Breaking News
+        18. @FEWSNET            '80797182'          FEWS NET                Famine Early Warning Systems Network
+        19. @ECDC_Outbreaks     '2754484003'        ECDC Outbreaks          European Centre for Disease Prevention and Control
+        20. @nytimesworld       '1877831'           New York Times World
+        21. @TheWarMonitor      '1952855342'        WarMonitor
+        22. @Strat2Intel        '4048091663'        Strat 2 Intel           Intelligence arm of Strategic Sentinel
+        23. @StratSentinal      '789303014192402432'Strategic Sentinel      
+        
+        deleted sources                         reason
+            @markets                            messages are retweeted by @business
+            @reuters                            messages are repeated multiple times daily
 """
 accounts = ['34713362', '428333', '18767649', '23484039', '346569891', '804556370', '81075524', '51241574',
             '2566535282', '380648579', '189305014', '87416722', '94119095', '26574283', '18424289', '16666806',

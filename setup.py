c32ef6964188f2b845c6938b276036679a6d6800
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-11 11:12:11 -0600

    added code block that creates stock_markets table in database

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class InformationCollectorSetup:
         conn = sqlite3.connect(database)
         c = conn.cursor()
 
-        # this creates the table that takes in data from the tweet_parser
+        # this creates the table that takes in data from the tweet_collector
         table_tweets = 'CREATE TABLE IF NOT EXISTS tweets (name TEXT, screename TEXT, ' \
                        'published REAL, tweet TEXT, coordinates_long REAL, coordinate_lat REAL,  ' \
                        'place_sw_point_long REAL, place_sw_point_lat REAL, place_nw_point_long REAL, ' \
@@ -29,9 +29,14 @@ class InformationCollectorSetup:
                        'place_se_point_long REAL, place_se_point_lat REAL, collector TEXT, url REAL)'
         c.execute(table_tweets)
 
-        # this creates the table that takes in data from the rss_feed_parser
+        # this creates the table that takes in data from the rss_collector
         table_rss = 'CREATE TABLE IF NOT EXISTS rss (name TEXT, published REAL, ' \
                     'imported TEXT, title TEXT, summary TEXT, url TEXT)'
         c.execute(table_rss)
+
+        # this code block creates the table that takes in data from the stock_collector
+        table_stock_markets = 'CREATE TABLE IF NOT EXISTS stock_markets (name TEXT, symbol TEXT, country TEXT, ' \
+                              'published REAL, imported REAL, open REAL, high REAL, low REAL, close REAL)'
+        c.execute(table_stock_markets)
 
         c.close()

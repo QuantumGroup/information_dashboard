b67daa96883b0a15f6d8c04e16464f41d787c5bd
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-14 17:09:52 -0600

    removed SQLIte database from VCS

--- dissemination/sms_alerts/alerts.py
+++ dissemination/sms_alerts/alerts.py
@@ -1,3 +1,6 @@
 """
-This class will be used to send alert messages via SMS.
+This class sends SMS alerts to selected phone numbers when CRITIC warnings are identified. These are errors that need to
+be immediately addressed, as they either stop system functionality or are credible threats to data integrity.
+
+Note: SMS messages should only be used in important circumstances, as these pose a small but actual cost per use.
 """

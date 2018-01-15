cb2fc3f3ea7eedc9de83359e11cb6724759dd663
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-15 09:30:50 -0600

    removed SMS alert call from control and gave alert function to individual scripts instead

--- control.py
+++ control.py
@@ -10,7 +10,6 @@ from collection.real_time_collectors import twitter_location_collector, twitter_
     twitter_track_collector
 import collection.batch_collectors.rss_collector as rss_collector
 import collection.batch_collectors.stock_collector as stock_collector
-import dissemination.sms_alerts.alerts as sms_alerts
 
 """
 sets up whether the setup script is needed 
@@ -38,15 +37,6 @@ sets up files, if necessary
 setup = setup.InformationCollectorSetup()
 if setup_required is True:
     setup.initiate()
-
-"""
-sets up SMS alert class
-"""
-alerts = sms_alerts.SMS_alerts()
-
-CRITIC_alert = alerts.send_sms('CRITIC alert\n'
-                               '------------\n'
-                               'system failure')
 
 """
 sets up variables for Twitter location collection: this is to be a list of the four corners of a bounded box. Per the 

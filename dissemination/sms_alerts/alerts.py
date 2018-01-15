fbc2a711b1e6f50e75a9a5ee0317ec03f612e0ec
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-15 09:29:43 -0600

    separated SMS alert API callback method from message creation and sending method

--- dissemination/sms_alerts/alerts.py
+++ dissemination/sms_alerts/alerts.py
@@ -11,7 +11,7 @@ class SMS_alerts:
     def __init__(self):
         pass
 
-    def send_sms(self, message):
+    def prepare_sms(self, message_text):
         # Python library import
         from twilio.rest import Client
         # local file imports
@@ -19,8 +19,31 @@ class SMS_alerts:
 
         client = Client(keys.twilio_account_sid, keys.twilio_auth_token)
 
-        message = client.messages.create(
-            to=keys.personal_cell_phone,
-            from_=keys.twilio_outbound_phone,
-            body=message)
+        client.messages.create(to=keys.personal_cell_phone,
+                               from_=keys.twilio_outbound_phone,
+                               body=message_text)
 
+    def critic_sms(self, failure_message):
+        """
+        Used whenever there is a critical failure in the script that requires immediate attention.
+        :param failure_message: str
+            The string can be passed from wherever the method is called to provide more information about where the
+            failure occured
+        :return: None
+        """
+
+        if failure_message:
+            pass
+        else:
+            failure_message = ''
+
+        # the following is a standard message passed regardless of the nature of error
+        message_text = 'GRID critical failure\n' \
+                       '---------------------\n' \
+                       'An error has occurred.\n' \
+                       '%s' % failure_message
+
+        self.prepare_sms(message_text)
+
+# sms = SMS_alerts()
+# alert = sms.critic_sms('a test message')

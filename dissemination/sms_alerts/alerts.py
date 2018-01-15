b568d23a1c3b9d2a1d0991d825a9e577bf9823da
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-14 18:03:53 -0600

    added scaffolding for SMS alert script

--- dissemination/sms_alerts/alerts.py
+++ dissemination/sms_alerts/alerts.py
@@ -4,8 +4,27 @@ be immediately addressed, as they either stop system functionality or are credib
 
 Note: SMS messages should only be used in important circumstances, as these pose a small but actual cost per use.
 """
-# Python library import
-import twilio
-# local file imports
-import _keys_and_secrets as keys
 
+
+class SMS_alerts:
+
+    def send_sms(self, message):
+        # Python library import
+        from twilio.rest import Client
+        # local file imports
+        import _keys_and_secrets as keys
+
+        client = Client(keys.twilio_account_sid, keys.twilio_auth_token)
+
+        message = client.messages.create(
+            to=keys.personal_cell_phone,
+            from_=keys.twilio_outbound_phone,
+            body=message)
+
+        print(message.sid)
+
+message = 'another test message'
+
+alerts = SMS_alerts()
+
+alerts.send_sms(message)

e25a670a5a652235d7bad339ee8e2c243acf17ce
Hector Torres <blackarrow06@users.noreply.github.com>
2018-01-17 21:54:11 -0600

    created class that takes error messages and 1) prints them to console, 2) saves them to a log, and if needed 3) sends them via SMS

---
+++ error.py
@@ -0,0 +1,49 @@
+"""
+This class contains code that will be called in the case of errors in other scripts
+"""
+
+
+class error:
+
+    def if_error(self, e, full_e, method_name, failure_type, sms=False):
+        """
+        This method takes in errors from other methods and prints them to console and sends them as text to a specified
+        phone number.
+        :param e: Exception
+        the exception that is raised by the error
+        :param full_e: str
+        this the is full stack trace for the error
+        :param method_name: str
+        the name of the method where the exception has occurred
+        :param failure_type: str
+        the specific type of error designed to be caught by the try/except block
+        :param sms: bool
+        this specifies whether SMS alerts should be sent when this method is trigger: SHOULD BE USED SPARINGLY!
+        :return:
+        """
+        # Python library imports
+        import datetime
+        import time
+        # local file imports
+        import dissemination.sms_alerts.alerts as alerts
+
+        # acquires the current time
+        current_time_int = int(time.time())
+        current_time_struct = time.gmtime(current_time_int)
+        current_time = str(datetime.datetime.fromtimestamp(time.mktime(current_time_struct)))
+
+        # instantiates SMS alerts class
+        sms_alert = alerts.SMS_alerts()
+
+        # creates multi-purpose error message
+        message = ("error at: %s\n"
+                   "error in: %s\n"
+                   "error type: %s\n"
+                   "exception: %s\n\n"
+                   % (current_time, method_name, failure_type, str(e)))
+
+        print(message)
+        with open('error_log.txt', 'a') as f:
+            f.write(full_e)
+        if sms is True:
+            sms_alert.critic_sms(message)
